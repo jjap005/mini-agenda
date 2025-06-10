c9d4f4117c93b2b68257b32d731acf7e1b86846d
Junior Aguilar <jjap@Juniors-MacBook-Pro.local>
2025-06-11 01:52:59 +0200

    basic config to tests - mini test

---
+++ test/test_helper.rb
@@ -0,0 +1,8 @@
+require 'minitest/autorun'
+require_relative '../lib/gen_server'
+require_relative '../commands/add_contact'
+require_relative '../commands/remove_contact'
+require_relative '../queries/find_contact'
+require_relative '../queries/list_contacts'
+require_relative '../domains/contacts/contact'
+require_relative '../validators/email_validator'
