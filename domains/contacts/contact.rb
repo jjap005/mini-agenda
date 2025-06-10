75dc18dc2a7b4309034177fb14c498729adac4b4
Junior Aguilar <jjap@Juniors-MacBook-Pro.local>
2025-06-11 00:38:18 +0200

    basic struct Contact (model)

--- domains/contacts/contact.rb
+++ domains/contacts/contact.rb
@@ -1,6 +1,13 @@
 module MiniAgenda::Domains
   module Contacts
     class Contact
+      attr_reader :name, :phone, :email
+
+      def initialize(name:, phone:, email:)
+        @name = name.strip
+        @phone = phone.strip
+        @email = email.strip
+      end
     end
   end
 end
